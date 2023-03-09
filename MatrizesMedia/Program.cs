86a9c15a7b3122f132558395abe871dec2a714a3
GBFerro <giovani.ferro@usp.br>
2023-03-08 22:47:28 -0300

    Add project files.

---
+++ MatrizesMedia/Program.cs
@@ -0,0 +1,18 @@
+﻿double[,] table = new double[5, 3];
+Random rnd = new Random();
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 2; j++)
+    {
+        int num = rnd.Next(100);
+        table[i, j] = num;
+    }
+    table[i, 2] = (table[i, 1] + table[i, 0]) / 2;
+    for (int k = 0;  k < 3; k++)
+    {
+    Console.Write(table[i, k] + " ");
+    } 
+    Console.WriteLine("\n");
+}
+
