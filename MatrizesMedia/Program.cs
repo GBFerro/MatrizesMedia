72039fb607814d18771cad4f8eb9026b45cace5e
GBFerro <giovani.ferro@usp.br>
2023-03-09 11:17:58 -0300

    refatorando para retornar numeros reais

--- MatrizesMedia/Program.cs
+++ MatrizesMedia/Program.cs
@@ -5,14 +5,14 @@ for (int i = 0; i < 5; i++)
 {
     for (int j = 0; j < 2; j++)
     {
-        int num = rnd.Next(100);
+        double num = rnd.NextDouble()*100;
         table[i, j] = num;
     }
     table[i, 2] = (table[i, 1] + table[i, 0]) / 2;
-    for (int k = 0;  k < 3; k++)
+    for (int k = 0; k < 3; k++)
     {
-    Console.Write(table[i, k] + " ");
-    } 
+        Console.Write(table[i, k].ToString("f2") + " | ");
+    }
     Console.WriteLine("\n");
 }
 
